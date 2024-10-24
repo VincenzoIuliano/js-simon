faeed58ef5ddb9b2cadc7abc962b6c7fe205caa5
VincenzoIuliano <bubucane@outlook.it>
2024-10-24 15:41:55 +0200

    ADD: initial status

---
+++ script.js
@@ -0,0 +1,19 @@
+let numberOne = document.getElementById('n1')
+numberOne = numberOne.innerHTML = Math.round(Math.random() * 100);
+console.log(numberOne)
+
+let numberTwo = document.getElementById('n2')
+numberTwo = numberTwo.innerHTML = Math.round(Math.random() * 100);
+console.log(numberTwo)
+
+let numberThree = document.getElementById('n3')
+numberThree = numberThree.innerHTML = Math.round(Math.random() * 100);
+console.log(numberThree)
+
+let numberFour = document.getElementById('n4')
+numberFour = numberFour.innerHTML = Math.round(Math.random() * 100);
+console.log(numberFour)
+
+let numberFive = document.getElementById('n5')
+numberFive = numberFive.innerHTML = Math.round(Math.random() * 100);
+console.log(numberFive)
